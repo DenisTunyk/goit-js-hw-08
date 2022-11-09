872e3156b105d57b6c35d11c4486fa699e21b908
DenisTunyk <tundenis43@gmail.com>
2022-11-09 13:40:38 +0200

    finish project

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+
+
+player.setCurrentTime(load(LOCALSTORAGE_KEY).seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(timeOnNow, 1000));
+
+function timeOnNow(e) {
+    save(LOCALSTORAGE_KEY, e);
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,63 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector(".js-input");
+const message = document.querySelector(".js-message");
+
+form.addEventListener("input", throttle(onFormInput, 500))
+form.addEventListener("submit", onFormSubmit)
+const storageValue = {}
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+//form.firstElementChild.firstElementChild.value = "888";
+
+if (load(LOCALSTORAGE_KEY).email) {
+    //console.log(form.firstElementChild);
+    input.value = load(LOCALSTORAGE_KEY).email;
+}
+if (load(LOCALSTORAGE_KEY).message) {
+    //console.log(form.firstElementChild);
+    message.value = load(LOCALSTORAGE_KEY).message;
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log("Відправлення форми");
+    console.log("Email: ", input.value);
+    console.log("Message: ", message.value);
+    input.value = "";
+    message.value = "";
+    localStorage.removeItem(LOCALSTORAGE_KEY)
+}
+
+function onFormInput(e) {
+    
+    storageValue[e.target.name] = e.target.value;
+    //save(LOCALSTORAGE_KEY, JSON.stringify(storageValue))
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storageValue));
+    // storageValue[e.target.name] = e.target.value;
+
+    // if (e.target.name === "email") {
+    //     storageValue.email = e.target.value;
+    //     save(LOCALSTORAGE_KEY, JSON.stringify(storageValue))
+    //     console.log(JSON.stringify(storageValue));
+    // } else if (e.target.name === "message") {
+    //     storageValue.message = e.target.value;
+    //     save(LOCALSTORAGE_KEY, JSON.stringify(storageValue))
+    //     console.log(JSON.stringify(storageValue));
+    // }
+    //console.log(storageValue);
+}
+
+//console.log(email);
